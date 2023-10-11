1a64b0560ad665d1911cacbb41c2b4fb7237e225
Awa DIAGNE <awa94.diagne@gmail.com>
2023-10-11 09:46:26 +0000

    #44 Test the terra home on terratowns (#45)

--- main.tf
+++ main.tf
@@ -8,33 +8,35 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
 
-/*
 module "terrahouse_aws" {
   source = "./modules/terrahouse_aws"
-  user_uuid = var.user_uuid
-  bucket_name = var.bucket_name
+  user_uuid = var.teacherseat_user_uuid
   index_html_filepath = var.index_html_filepath
   error_html_filepath = var.error_html_filepath
   content_version = var.content_version
   assets_path = var.assets_path
 }
-*/
 
 resource "terratowns_home" "home" {
-  name = "How to play Arcanum in 2023!"
+  name = "One Piece Manga from Eiichiro Oda"
   description = <<DESCRIPTION
-Arcanum is a game from 2001 that shipped with alot of bugs.
-Modders have removed all the originals making this game really fun
-to play (despite that old look graphics). This is my guide that will
-show you how to play arcanum without spoiling the plot.
+"One Piece" is a highly popular and long-running Japanese manga series created by 
+Eiichiro Oda. The story follows Monkey D. Luffy, a young and enthusiastic pirate with 
+the ability to stretch his body like rubber after eating a Devil Fruit. Luffy aspires 
+to become the Pirate King by finding the ultimate treasure, known as the One Piece, 
+hidden at the end of the Grand Line. Along the way, he forms a diverse and loyal crew, 
+the Straw Hat Pirates, and they embark on a grand adventure, encountering a wide array 
+of characters, islands, and challenges. 
+"One Piece" is celebrated for its intricate world-building, memorable characters, and 
+a mix of action, humor, and heartwarming moments, making it one of the most beloved 
+and successful manga series of all time.
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  town = "missingo"
   content_version = 1
 }
